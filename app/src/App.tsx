a5616853bfc782496f44a3a71f8a5fac550eb217
jb1361 <justinbutler4@hotmail.com>
2018-11-18 22:41:45 -0500

    Add react navigation

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -11,22 +11,13 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
-const instructions = Platform.select({
-    ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-    android:
-        'Double tap R on your keyboard to reload,\n' +
-        'Shake or press menu button for dev menu',
-});
 
-interface Props {}
-export default class App extends Component<Props> {
+class App extends React.Component {
     render() {
-        throw new Error('This is an error to test Sentry');
         return (
             <View style={styles.container}>
                 <Text style={styles.welcome}>Welcome to React Native!!!</Text>
                 <Text style={styles.instructions}>To get started, edit App.tsx</Text>
-                <Text style={styles.instructions}>{instructions}</Text>
             </View>
         );
     }
@@ -50,3 +41,5 @@ const styles = StyleSheet.create({
         marginBottom: 5,
     },
 });
+
+export default App;
